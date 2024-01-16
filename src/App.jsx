66a5ae3081978f0dfa731b558cbb59a8443947d8
[tushar khatri] <tusharkhatri8193@gmail.com>
2024-01-16 11:08:17 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "./components/HeroSection"
+import NavBar from "./components/NavBar"
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <HeroSection />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import profile from "../assets/profile.jpeg";
+import { LiaLinkedin } from "react-icons/lia";
+import { FiGithub } from "react-icons/fi";
+
+const HeroSection = () => {
+
+  return (<div data-testid="HeroSectionID" className="bg-stone-50">
+    {/* Write your code here */}
+  </div>);
+};
+
+export default HeroSection;
